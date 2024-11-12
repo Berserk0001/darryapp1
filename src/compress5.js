438c5e903d393d642f9001ccb66075e6c79c5a28
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-12 21:31:12 +0530

    Update compress5.js

--- src/compress5.js
+++ src/compress5.js
@@ -26,7 +26,7 @@ function compress(req, res, input) {
    * |x-bytes-saved  |Saved bandwidth from original photo|OriginSize - Compressed Size|
    */
 
-  input.body.pipe(sharp()
+  input.pipe(sharp()
     .resize(null, 12480, {
         withoutEnlargement: true
       })
